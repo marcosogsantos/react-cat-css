2abb942645ca3275c9a0f48b9b7092174d1a47b1
marcosogsantos <51763881+marcosogsantos@users.noreply.github.com>
2019-06-26 14:58:40 -0300

    Add files via upload

---
+++ precache-manifest.af467c9a99ce8df9c9d0c2ad90018c0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63517c821691e6dcf4d46e624d9e3e15",
+    "url": "/react-cat-css/index.html"
+  },
+  {
+    "revision": "18d9ee1746997aefd06e",
+    "url": "/react-cat-css/static/css/main.39cd1487.chunk.css"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/react-cat-css/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "18d9ee1746997aefd06e",
+    "url": "/react-cat-css/static/js/main.99915c47.chunk.js"
+  },
+  {
+    "revision": "ed8b421463047d6ea884",
+    "url": "/react-cat-css/static/js/runtime~main.71386406.js"
+  }
+]);
